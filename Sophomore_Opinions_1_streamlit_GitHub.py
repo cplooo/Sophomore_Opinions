19e01a875faebb80d92ec00fb6de211fd20cd0b7
cplooo <cplo@kimo.com>
2024-01-30 17:07:26 +0800

    Add files via upload

--- Sophomore_Opinions_1_streamlit_GitHub.py
+++ Sophomore_Opinions_1_streamlit_GitHub.py
@@ -491,109 +491,56 @@ st.markdown("##")  ## 更大的间隔
 
 
 
-# ###### Part2-9  您工讀最主要的原因為何?
-# df_sophomore.iloc[:,21] ##  9.您工讀最主要的原因為何?
-# ##### 将字符串按逗号分割并展平
-# #split_values = df_sophomore.iloc[:,19].str.split(',| |，|、').explode()
-# # ##### 过滤出只包含'是'或'否'的子字符串
-# # filtered_values = split_values[split_values.isin(['是', '否'])]
-# ##### 计算不同子字符串的出现次数
-# #value_counts = split_values.value_counts()
-# value_counts = df_sophomore.iloc[:,21].value_counts()
-# ##### 计算不同子字符串的比例
-# proportions = value_counts / value_counts.sum()
+###### Part2-9  您工讀最主要的原因為何?
+with st.expander("您工讀最主要的原因為何:"):
+    # df_sophomore.iloc[:,21] ##  9.您工讀最主要的原因為何?
+    column_title.append(df_sophomore.columns[21][2:])
+    ##### 将字符串按逗号分割并展平
+    split_values = df_sophomore.iloc[:,21].str.split(',').explode()
+    ##### 计算不同子字符串的出现次数
+    value_counts = split_values.value_counts()
+    ##### 计算不同子字符串的比例
+    proportions = value_counts / value_counts.sum()
+    ##### 轉換成 numpy array
+    value_counts_numpy = value_counts.values
+    proportions_numpy = proportions.values
+    items_numpy = proportions.index.to_numpy()
+    ##### 创建一个新的DataFrame来显示结果
+    result_df = pd.DataFrame({'項目':items_numpy, '人數': value_counts_numpy,'比例': proportions_numpy.round(4)})
+    ##### 存到 list 'df_streamlit'
+    df_streamlit.append(result_df)  
+    ##### 使用Streamlit展示DataFrame，但不显示索引
+    st.write("您工讀最主要的原因為何:", result_df.to_html(index=False), unsafe_allow_html=True)
+    st.markdown("##")  ## 更大的间隔
+    ##### 使用Streamlit畫圖
+    #### 設置中文顯示
+    # matplotlib.rcParams['font.family'] = 'Microsoft YaHei'
+    # matplotlib.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    matplotlib.rcParams['font.family'] = 'Noto Sans CJK JP'
+    matplotlib.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
+    #### 创建图形和坐标轴
+    plt.figure(figsize=(11, 8))
+    #### 绘制条形图
+    plt.barh(result_df['項目'], result_df['人數'])
+    #### 標示比例數據
+    for i in range(len(result_df['項目'])):
+        plt.text(result_df['人數'][i]+1, result_df['項目'][i], f'{result_df.iloc[:, 2][i]:.0%}', fontsize=16)
+    #### 添加一些图形元素
+    plt.title('您工讀最主要的原因為何', fontsize=16)
+    plt.xlabel('人數', fontsize=16)
+    #plt.ylabel('本校現在所提供的資源或支援事項')
+    #### 调整x轴和y轴刻度标签的字体大小
+    plt.tick_params(axis='both', labelsize=16)  # 同时调整x轴和y轴
+    plt.legend()
+    #### 显示网格线
+    plt.grid(True, linestyle='--', linewidth=0.5, color='gray')
+    #### 显示图形
+    ### 一般顯示
+    # plt.show()
+    ### 在Streamlit中显示
+    st.pyplot(plt)
+st.markdown("##")  ## 更大的间隔    
 
-# #%% (九) 以下
-# ##### 创建一个新的DataFrame来显示结果
-# result_df_1st = pd.DataFrame({
-#     '人數': value_counts,
-#     '比例': proportions.round(3)
-# })
-# print('工讀最主要原因:')
-# print(result_df_1st)
-# '''
-# 工讀最主要原因:
-#                      人數     比例(由大至小)
-# 負擔生活費               379  0.449
-# 不須負擔生活費但想增加零用錢      279  0.330
-# 為未來工作累積經驗            98  0.116
-# 學習應對與表達能力            65  0.077
-# 增加人脈                  9  0.011
-# 無                     1  0.001
-# 因為無聊想找事做              1  0.001
-# 太無聊                   1  0.001
-# 存錢繳學貸                 1  0.001
-# 以上皆是                  1  0.001
-# 生活費、日常開銷、累積經驗、學習耐性    1  0.001
-# 體驗人生                  1  0.001
-# 還時數                   1  0.001
-# 學中文，練習講中文             1  0.001
-# 找好未來出路                1  0.001
-# 生活費，學費                1  0.001
-# 體驗生活                  1  0.001
-# 補貼家用                  1  0.001
-# 不想跟家裡拿生活費想靠自己賺        1  0.001
-# 玩                     1  0.001
-# '''
-# #### 將 index 變column
-# result_df_1st_r = result_df_1st.reset_index()
-# #### 重新命名新的column
-# result_df_1st_r.rename(columns={'index': '工讀最主要原因'}, inplace=True)
-# print(result_df_1st_r)
-
-# '''
-#               工讀最主要原因   人數     比例
-# 0                負擔生活費  379  0.449
-# 1       不須負擔生活費但想增加零用錢  279  0.330
-# 2            為未來工作累積經驗   98  0.116
-# 3            學習應對與表達能力   65  0.077
-# 4                 增加人脈    9  0.011
-# 5                    無    1  0.001
-# 6             因為無聊想找事做    1  0.001
-# 7                  太無聊    1  0.001
-# 8                存錢繳學貸    1  0.001
-# 9                 以上皆是    1  0.001
-# 10  生活費、日常開銷、累積經驗、學習耐性    1  0.001
-# 11                體驗人生    1  0.001
-# 12                 還時數    1  0.001
-# 13           學中文，練習講中文    1  0.001
-# 14              找好未來出路    1  0.001
-# 15              生活費，學費    1  0.001
-# 16                體驗生活    1  0.001
-# 17                補貼家用    1  0.001
-# 18      不想跟家裡拿生活費想靠自己賺    1  0.001
-# 19                   玩    1  0.001
-# '''
-# #%% (九) 以上
-
-# #%% (九圖) 以下
-# #### 圖：工讀主要原因
-# ### 创建图形和坐标轴
-# plt.figure(figsize=(11, 8))
-# bar_num = 5
-# ### 绘制条形图
-# plt.barh(result_df_1st_r['工讀最主要原因'][:bar_num], result_df_1st_r['人數'][:bar_num])
-# #plt.barh(series.index, series)
-# ### 標示數據
-# #for i in range(len(result_df_2nd_r['工讀次要原因'])):
-# for i in range(bar_num):
-#     plt.text(result_df_1st_r['人數'][i]+5, result_df_1st_r['工讀最主要原因'][i], f'{result_df_1st_r.iloc[:, 2][i]:.0%}', fontsize=16)
-
-# ### 添加一些图形元素
-# plt.title('工讀最主要原因', fontsize=16)
-# plt.xlabel('人數', fontsize=16)
-# #plt.ylabel('本校現在所提供的資源或支援事項')
-# ### 调整x轴和y轴刻度标签的字体大小
-# plt.tick_params(axis='both', labelsize=16)  # 同时调整x轴和y轴
-
-# plt.legend()
-# ### 显示网格线
-# plt.grid(True, linestyle='--', linewidth=0.5, color='gray')
-# ### 显示图形
-# plt.show()
-
-
-# #%% (九圖) 以上
 
 
 
